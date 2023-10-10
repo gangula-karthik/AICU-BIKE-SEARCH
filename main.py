6bc288e3aee8bb15beb81f9b0c5ab3505a8e98d9
Foo Yong Xiang <yongxiangfoo@gmail.com>
2023-10-10 21:37:42 +0800

    Cleaning up the files

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080)
