e2f89a0a12f06a0a624fff4cdf57f510944bc47a
Philander1 <philanderong2005@gmail.com>
2023-10-11 18:06:17 +0800

    Philander edit 2!

--- app/app.py
+++ app/app.py
@@ -4,15 +4,20 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 app = Flask(__name__)
 
 
+# base template testing
+@app.route('/test')
+def index():
+    return render_template('index.html')
+
+
 @app.route('/')
 def login():
     return render_template('login.html')
 
 
-# base template testing
-@app.route('/test')
-def index():
-    return render_template('index.html')
+@app.route('/qr_code')
+def qr_code():
+    return render_template('qr_code.html')
 
 
 if __name__ == '__main__':
