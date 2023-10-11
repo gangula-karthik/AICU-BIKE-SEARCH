0698f1cfd3dc15fb51d12829c54fe80d4dfbce0e
Philander1 <philanderong2005@gmail.com>
2023-10-11 16:05:48 +0800

    First Philander edit!

--- app/app.py
+++ app/app.py
@@ -5,6 +5,12 @@ app = Flask(__name__)
 
 
 @app.route('/')
+def login():
+    return render_template('login.html')
+
+
+# base template testing
+@app.route('/test')
 def index():
     return render_template('index.html')
 
