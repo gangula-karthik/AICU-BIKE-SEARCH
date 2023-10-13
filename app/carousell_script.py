abc6f80c7c914bb532807a42eabad560b1b3192a
LEAP <56480632+gangula-karthik@users.noreply.github.com>
2023-10-13 12:38:21 +0800

    add: final cleaned csv file

---
+++ app/carousell_script.py
@@ -0,0 +1,18 @@
+import requests
+import pandas as pd
+import time
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+import re
+from datetime import datetime, timedelta
+import os
