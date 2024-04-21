92fb18df01ca6084af45a8f4a7894d2772e0c7b1
LEAP <56480632+gangula-karthik@users.noreply.github.com>
2024-04-21 12:06:53 +0800

    add: clean code

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -10,8 +10,7 @@ import { Analytics } from "@vercel/analytics/react";
 import { Toaster } from "sonner";
 
 
-const clash = localFont({
-  src: "../styles/ClashDisplay-Semibold.otf",
+const clash = localFont({ src: "../styles/ClashDisplay-Semibold.otf",
   variable: "--font-clash",
 });
 
@@ -63,29 +62,6 @@ export default function RootLayout({
         <main className="flex min-h-screen w-full flex-col items-center justify-center py-32">
           {children}
         </main>
-        <div className="absolute w-full py-5 text-center">
-          <p className="text-gray-500">
-            A project by{" "}
-            <a
-              className="font-semibold text-gray-600 underline-offset-4 transition-colors hover:underline"
-              href="https://twitter.com/steventey"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Karthik
-            </a>
-          </p>
-          <a
-            href="#"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="mx-auto mt-2 flex max-w-fit items-center justify-center space-x-2 rounded-lg border border-gray-200 bg-white px-6 py-2 transition-all duration-75 hover:scale-105"
-          >
-            <BuyMeACoffee className="h-6 w-6" />
-            <p className="font-medium text-gray-600">Buy me a coffee</p>
-          </a>
-        </div>
-        <Analytics />
       </body>
     </html>
   );
